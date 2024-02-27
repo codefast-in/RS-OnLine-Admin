a632b6fd4aa96c46d50bf3a87da4a29faaacebae
code-fast <sachinspindofficial@gmail.com>
2024-02-27 17:07:03 +0530

    Update Pai_Chart.tsx

--- src/components/Chart/Pai_Chart.tsx
+++ src/components/Chart/Pai_Chart.tsx
@@ -3,32 +3,16 @@ import Chart from "react-apexcharts";
 import { Card } from "../ui/card";
 
 const Paichart = () => {
-  const [options, setOptions] = useState({
-    chart: {
-      type: "donut"
-    },
-    responsive: [
-      {
-        breakpoint: 480,
-        options: {
-          chart: {
-            width: 200
-          },
-          legend: {
-            position: "bottom"
-          }
-        }
-      }
-    ]
-  });
-
+  const [options, setOptions] = useState({ });
+  
+  labels: ['A', 'B', 'C', 'D', 'E']
   const [series, setSeries] = useState([44, 55, 41, 17, 15]);
 
   return (
     <Card className="app w-full">
       <div className="row ">
         <div className="mixed-chart">
-          <Chart options={options} series={series} type="donut" width="430" />
+          <Chart  options={options} series={series} type="donut" width="430" />
         </div>
       </div>
     </Card>
